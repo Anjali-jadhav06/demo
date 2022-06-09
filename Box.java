903b2730185410a12967ae8b045b9322fa5b77ed
Anjali-jadhav06 <106802650+Anjali-jadhav06@users.noreply.github.com>
2022-06-09 18:07:11 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,27 @@
+package com.anjali;
+
+public class Box {
+	int length, breadth, height;
+	Box(int I,int b,int h)
+	{
+		length=I;
+		breadth=b;
+		height=h;
+	}
+	public void display()
+	{
+		int volume=length*breadth*height;
+		System.out.println("Volume of Box is"+volume);
+	}
+
+
+
+class Boxdemo
+{
+public static void main(string args[])
+{
+	Box b=new Box(10,20,30);
+	b.display();
+}
+}
+}
